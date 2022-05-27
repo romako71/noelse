8897e7d55f2a207837a73be1778638d0a23bd32e
Romako <romako1@yandex.ru>
2022-05-27 08:38:14 +0300

    25 todo

--- mod25/carma.py
+++ mod25/carma.py
@@ -1,37 +1,53 @@
 import random
 
-
 class Bodhisattva:
 
     def __init__(self, name):
-        self.name = name
-        self.carma = 0
+        self.__name = name
+        self.__carma = 0
 
     def __str__(self):
-        return '{} имеет {} очков кармы'.format(self.name, self.carma)
+        return '{} имеет {} очков кармы'.format(self.__name, self.__carma)
 
     def change_carma(self, points):
-        self.carma += points
+        self.__carma += points
+
+    def get_carma(self):
+        return self.__carma
 
 
-def one_day(file_name):
+def one_day(file_name, next_day):
     try:
         if random.randint(1, 10) == 1:
-            raise BaseException
-            if error_type == 1:
-                raise KillErrorExeption()
+            raise Exception
+        return random.randint(1, 7)
+    except:
+        fate = random.randint(1, 5)
+        # TODO тут надо не просто сообщение, а raise-ить ошибки, ошибки надо создать свои, как это сделано тут https://github.com/tortoise/tortoise-orm/blob/develop/tortoise/exceptions.py
+        if fate == 1:
+            message = 'KillError'
+        elif fate == 2:
+            message = 'DrunkError'
+        elif fate == 3:
+            message = 'CarCrashError'
+        elif fate == 4:
+            message = 'GluttonyError'
+        elif fate == 5:
+            message = 'DepressionError'
+        else:
+            message = 'GlobalKarmaError'
+        file_name.write('День {}: {}\n'.format(next_day, message))
+        return 0
 
-    carma_points = random.randint(1, 7)
-    bodhisattva.change_carma(carma_points)
 
 error_type = random.randint(1, 7)
 bodhisattva = Bodhisattva('Tom')
-day = 0
+trial_day = 0
 
-try:
-    with open('carma.log', 'w') as error_file:
-        while bodhisattva.carma < 500:
-            day += 1
-            one_day()
+with open('carma.log', 'w', encoding='utf-8') as error_file:
+    while bodhisattva.get_carma() < 500:
+        trial_day += 1
+        plus_carma = one_day(error_file, trial_day)
+        bodhisattva.change_carma(plus_carma)
 
-print(f'Набрано {bodhisattva.carma} очков кармы. Поздравляем с просветлением на {day} день!')
+print(f'Набрано {bodhisattva.get_carma()} очков кармы. Поздравляем с просветлением на {trial_day} день!')
